e65d4d6c6043a016791a92a42a47c220f0b7931a
youn-somi <thaldlqslek@naver.com>
2026-05-14 11:10:08 +0900

    first commit

---
+++ src/main/java/com/my/member_app/dto/MemberDto.java
@@ -0,0 +1,30 @@
+package com.my.member_app.dto;
+
+import com.my.member_app.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberDto {
+    private Long id;
+    private String  name;
+    private int age;
+    private String address;
+
+    //Member Dto (Dto)- > 를 받아서 Member Entity  로 변환
+    public  static Member toEntity(MemberDto dto) {
+        //빈 깡통 Member 생성후 하나씩 넣어주고 리턴
+        Member member = new Member();
+        member.setId(dto.getId());
+        member.setName(dto.getName());
+        member.setAge(dto.getAge());
+        member.setAddress(dto.getAddress());
+        return member;
+
+    }
+    //Member Entity -> 를 받아서 MemberDto( DTO)로 변환
+
+}
